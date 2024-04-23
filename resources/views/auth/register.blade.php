2861b0279fa04b4430d8b9927d93556065315f7a
Stiven <villalobosbms@gmail.com>
2024-04-22 19:01:36 -0500

    mi primer commit

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,50 @@
+@extends('layouts.app')
+
+@section('titulo')
+    Registrate en DevStagram
+@endsection
+
+@section('contenido')
+    <div class="md:flex md:justify-center md:gap-10 md:items-center p-5">
+        <div class="md:w-6/12">
+            <img src="{{ asset('img/registrar.jpg')}}" alt="Imagen registro usuarios">
+        </div>
+
+        <div class="md:w-4/12 bg-white p-6 rounded-lg shadow-xl">
+            <form>
+                <div class="mb-5">
+                    <label for="name" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Nombre
+                    </label>
+                    <input type="text" name="name" id="name" placeholder="Nombre" class="border p-3 w-full rounded-lg">
+                </div>
+                <div class="mb-5">
+                    <label for="username" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Username
+                    </label>
+                    <input type="text" name="username" id="username" placeholder="Tu nombre de usuario" class="border p-3 w-full rounded-lg">
+                </div>
+                <div class="mb-5">
+                    <label for="email" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Email
+                    </label>
+                    <input type="email" name="email" id="email" placeholder="Ingresa tu correo" class="border p-3 w-full rounded-lg">
+                </div>
+                <div class="mb-5">
+                    <label for="password" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Password
+                    </label>
+                    <input type="password" name="password" id="password" placeholder="Ingresa tu contraseña" class="border p-3 w-full rounded-lg">
+                </div>
+                <div class="mb-5">
+                    <label for="password_confirmation" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Repetir Password
+                    </label>
+                    <input type="password" name="password_confirmation" id="password_confirmation" placeholder="Repetir Contraseña" class="border p-3 w-full rounded-lg">
+                </div>
+
+                <input type="submit" value="Crear Cuenta" class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white  rounded-lg"> 
+            </form>
+        </div>
+    </div>
+@endsection
